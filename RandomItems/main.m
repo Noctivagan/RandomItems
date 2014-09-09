b0cf1a607ffdf8d0954df1efbf3c58b2cb75aeab
Tom Anderson <tom@lupin.home>
2014-09-09 13:50:23 -0400

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Tom Anderson on 9/9/14.
+//  Copyright (c) 2014 Maritom LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
