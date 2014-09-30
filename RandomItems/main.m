88c5baa8f4b4d1ad1a326a9ebbf71c2c087ecab3
Tom Anderson <silverwing@noctivagan.com>
2014-09-30 17:47:16 -0400

    Initial load

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,14 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        BNRItem *backpack = [[BNRItem alloc] initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        BNRItem *calculator = [[BNRItem alloc] initWithItemName:@"Calculator"];
+        [items addObject:calculator];
+        backpack.containedItem = calculator;
+        backpack = nil;
+        calculator = nil;
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        NSLog(@"Setting items to nil...");
+        items = nil;
         
     }
     return 0;
